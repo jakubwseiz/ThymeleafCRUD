105c98540c8c3e9e99524b0e765fa1553d996332
Jakub Kozłowski <jakub.kozlowski@wseiz.edu.pl>
2023-09-07 22:15:38 +0200

    Make fields required in updateInvoice.html template.

--- src/main/java/com/example/ThymeleafCRUD/Repository/InvoiceRepository.java
+++ src/main/java/com/example/ThymeleafCRUD/Repository/InvoiceRepository.java
@@ -1,7 +1,6 @@
 package com.example.ThymeleafCRUD.Repository;
 
 import com.example.ThymeleafCRUD.Entities.Invoice;
-import com.example.ThymeleafCRUD.Entities.InvoiceItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
